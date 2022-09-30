9ecaa464ce59ed12e9ee883f0d2219c678334e3c
Maxim Faleev <m.faleev@innopolis.university>
2022-09-30 21:38:18 +0300

    refactoring

--- vocabulary_term_freq.py
+++ vocabulary_term_freq.py
@@ -13,51 +13,54 @@ from sklearn.metrics import mean_squared_error
 class TermFrequencyFeaturizer:
     MAX_TERM_FREQ = 23135751162
 
-    def __init__(self):
-        term_frequencies_dataset = self._load_term_frequency_dataset()
-        self.term2freq: Dict[str, int] = defaultdict(lambda: 0)
-        self.term2freq.update(term_frequencies_dataset.set_index("word").to_dict())
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self.term2freq: Dict[str, int] = self._load_term2freq_dict()
         nltk.download("punkt")
 
-    def _load_term_frequency_dataset(self) -> pd.DataFrame:
-        try:
-            term_frequencies_dataset = pd.read_csv("aux_data/unigram_freq.csv")
-        except IOError:
-            raise IOError(
-                "Guys, you need `unigram_freq.csv` dataset in aux_data/ folder"
-            )
-        return term_frequencies_dataset
+    def _load_term2freq_dict(self) -> Dict[str, int]:
+        term_frequencies = pd.read_csv("data/word_frequencies/unigram_freq.csv")
+        term2freq: Dict[str, int] = defaultdict(lambda: 0)
+        term2freq.update(term_frequencies.set_index("word").to_dict()['count'])
+        return term2freq
 
-    def featurize(self, texts: pd.Series, n_bins: int = 40) -> pd.DataFrame:
-        term_frequencies = texts.apply(self._compute_term_frequencies_from_text)
-        min_bin = 0
-        max_bin = np.log1p(self.MAX_TERM_FREQ)
-        bins = np.linspace(min_bin, max_bin, n_bins)
+    def featurize(self, texts: pd.Series, n_bins: int=40) -> pd.DataFrame:
+        """Extracts features from the text in the form of histogram of word frequencies
+
+        Logarithm operation is applied to the frequencies for the sake of distribution
+        normality.
+        """
+        bins = self._make_bins(n_bins)
         feature_names = [
             f"bin_{round(bins[i], 1)}_{round(bins[i+1], 1)}"
             for i in range(len(bins) - 1)
         ]
-        feature_values = []
-        for i, word_frequencies in enumerate(tqdm.tqdm(term_frequencies.values)):
-            word_frequencies_log = np.log1p(word_frequencies)
-            term_frequencies_histogram_values, __ = np.histogram(
-                word_frequencies_log, bins=bins
-            )
-            normalized_term_frequencies_histogram_values = (
-                term_frequencies_histogram_values / len(word_frequencies)
-            )
-            feature_values.append(normalized_term_frequencies_histogram_values)
-        feature_values = np.array(feature_values)
-        feature_df = pd.DataFrame(
-            feature_values, columns=feature_names, index=texts.index
-        )
+        feature_values = texts.apply(self._compute_word_frequency_histogram, bins=bins)
+        feature_df = pd.DataFrame(feature_values, columns=feature_names)
         return feature_df
+
+    def _make_bins(self, n_bins: int) -> np.ndarray:
+        min_bin = 0
+        max_bin = np.log1p(self.MAX_TERM_FREQ)
+        bins = np.linspace(min_bin, max_bin, n_bins)
+        return bins
+
+    def _compute_word_frequency_histogram(self, text: str, bins: np.ndarray) -> pd.Series:
+        term_frequencies: List[int] = self._compute_term_frequencies_from_text(text)
+        histogram_values: np.ndarray = self._build_histogram(term_frequencies, bins)
+        return pd.Series(histogram_values)
 
     def _compute_term_frequencies_from_text(self, text: str) -> List[int]:
         tokens = nltk.tokenize.word_tokenize(text)
         words = [token.lower() for token in tokens if token.isalpha()]
         word_frequencies = [self.term2freq[word] for word in words]
         return word_frequencies
+
+    def _build_histogram(self, values: List[int], bins: np.ndarray) -> np.ndarray:
+        values_log = np.log1p(values)
+        histogram, __ = np.histogram(values_log, bins=bins)
+        normalized_histogram = (histogram / len(values))
+        return normalized_histogram
 
 
 if __name__ == "__main__":

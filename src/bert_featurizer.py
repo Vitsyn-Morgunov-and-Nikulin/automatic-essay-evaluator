a16ec532b63522c320b541bd990fbd6a1b64e7bc
homomorfism <arsl.sh056@mail.ru>
2022-10-01 16:55:47 +0300

    Adding bert extractor class, that extracts [CLS] embedding from given text

--- src/bert_featurizer.py
+++ src/bert_featurizer.py
@@ -6,24 +6,32 @@ from transformers import AutoModel, AutoTokenizer
 class BertFeatureExtractor:
     """Extract [CLS] embedding feature from any untrained bert-like models"""
 
-    def __init__(self, model_name: str):
-        """
-        :param model_name: name of model from hugging face
-        """
+    def __init__(self, model_name: str, padding: str = 'max_length', max_length: int = 512):
         self.model_name = model_name
+        self.padding = padding
+        self.max_length = max_length
 
         self.model = AutoModel.from_pretrained(self.model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
 
     @torch.no_grad()
-    def extract_features(self, row: pd.Series):
-        text = row.full_text
+    def extract_features(self, data: pd.Series) -> pd.DataFrame:
+        """
+        Generates features, obtained from untrained bert model.
 
-        encoded = self.tokenizer(text=text, return_tensors='pt')
+        :param data: Series, that have index - id's from train/test dataframe and contains raw texts
+        :return: Dataframe, that have index - id's from data, and columns - bert features, and
+        """
+        texts = data.tolist()
+
+        encoded = self.tokenizer(text=texts, padding=self.padding, max_length=self.max_length, return_tensors='pt')
         output = self.model(**encoded)
-        cls_output = output['last_hidden_state'][0][0]
+        cls_output = output['last_hidden_state'][:, 0]
 
-        return pd.Series({
-            f'{self.model_name}_feature_{ii}': cls_output[ii].item()
-            for ii in range(len(cls_output))
-        })
+        column_names = [f"{self.model_name}_feat_{ii}" for ii in range(len(cls_output[0]))]
+
+        return pd.DataFrame(
+            data=cls_output.tolist(),
+            index=data.index,
+            columns=column_names
+        )
--- src/tests/test_feature_extractor.py
+++ src/tests/test_feature_extractor.py
@@ -6,15 +6,21 @@ from src.bert_featurizer import BertFeatureExtractor
 def test_feature_extractor():
     models = ['bert-base-uncased', 'distilbert-base-uncased-finetuned-sst-2-english']
 
-    row = pd.Series({
-        'full_text': "I think that students would benefit from learning at home,because "
-                     "they wont have to change and get up early in the morning to shower and "
-                     "do there hair. taking only classes helps them because at there house "
-                     "they'll be pay more attention. they will be comfortable at home. "
-    })
+    text_examples = pd.DataFrame({
+        'full_text': [
+            "I think that students would benefit from learning at home,because "
+            "they wont have to change and get up early in the morning to shower and "
+            "do there hair. taking only classes helps them because at there house "
+            "they'll be pay more attention. they will be comfortable at home. ",
+
+            "Have you ever solved a math problem in less than 30 seconds? "
+            "Math is important to many people. Even president Trump uses "
+            "math to buy food or pay back to important people. "
+        ]
+    }).full_text
 
     for model_name in models:
         feature_extractor = BertFeatureExtractor(model_name=model_name)
 
-        output_features = feature_extractor.extract_features(row)
-        assert len(output_features) == 768
+        output_features = feature_extractor.extract_features(text_examples)
+        assert len(output_features) == 2 and len(output_features.columns) == 768
